72f5f7e0b90d3b1df4421b8011d9af040266ab56
nicolevenachi <ludwing@unicauca.edu.co>
2023-12-24 14:01:31 -0500

    Meaningful mongoose errors -> Validations errors handled

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -12,23 +12,37 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
   // *** creating new error objects to handle errors from mongoose and from inner server state *** 
   let customError = {
-    // set default values for server errors
+    // set default values for server errors nad customAPIErrors
 
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR, // si ya hat algo, lo maniene
     msg: err.message || 'Something went wrong, try again later'
   }
 
+  // esto ahora lo maneja el customError
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
 
   // *** Checkings and updating de status code accoring to mongose error ***
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
 
   if (err.code && err.code === 11000){
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     customError.statusCode = StatusCodes.BAD_REQUEST
-  }
+  } // duplicated errors
 
+  if (err.name === "ValidationError") { 
+    // check for mutile keys/fields validation erros
+    customError.msg = Object.values(err.errors)
+      .map (item => item.message)
+      .join(', '); // creo un array con el mensaje de cada error, luego lo paso a string, separados por ,
+
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }  // validation errors
+
+
+  // *** Send response ***
+
+  // return res.status(customError.statusCode).json({ err }) // to check the entire object and create customizes messages/satus accordingly
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
